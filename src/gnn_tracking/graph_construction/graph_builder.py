e9da3eb4c69a78c37b1ca7836a0fd064eb700bd8
GageDeZoort <jgdezoort@gmail.com>
2022-08-12 16:59:56 -0400

    adding point cloud / graph builders

---
+++ src/gnn_tracking/graph_construction/graph_builder.py
@@ -0,0 +1,160 @@
+import os
+from os.path import join as join
+import numpy as np
+import pandas as pd
+import torch
+
+class GraphBuilder:
+    def __init__(self, indir, outdir, pixel_only=True,
+                 phi_slope_max=0.005, z0_max=200, 
+                 dR_max=1.7, uv_approach_max=0.0015,
+                 feature_names=["r", "phi", "z", "eta_rz", "u", "v", "layer"],
+                 feature_scale=np.array([1000.0, np.pi, 1000.0, 1, 1 / 1000.0, 1 / 1000.0])):
+        self.indir=indir
+        self.outdir=outdir
+        self.pixel_only = pixel_only
+        self.phi_slope_max=phi_slope_max
+        self.z0_max=z0_max
+        self.dR_max=dR_max
+        self.uv_approach_max=uv_approach_max
+        self.feature_names=feature_names
+        self.feature_scale=feature_scale
+        
+    def calc_dphi(self, phi1: np.ndarray, phi2: np.ndarray) -> np.ndarray:
+        """Computes phi2-phi1 given in range [-pi,pi]"""
+        dphi = phi2 - phi1
+        dphi[dphi > np.pi] -= 2 * np.pi
+        dphi[dphi < -np.pi] += 2 * np.pi
+        return dphi
+
+    def calc_eta(self, r: np.ndarray, z: np.ndarray) -> np.ndarray:
+        """Computes pseudorapidity
+        (https://en.wikipedia.org/wiki/Pseudorapidity)
+        """
+        theta = np.arctan2(r, z)
+        return -1.0 * np.log(np.tan(theta / 2.0))
+    
+    def get_dataframe(self, evt, evtid):
+        to_df = {'evtid': evtid}
+        for i, n in enumerate(self.feature_names):
+            to_df[n] = evt.x[:, i]
+        to_df['pt'] = evt.pt
+        to_df['particle_id'] = evt.particle_id
+        return pd.DataFrame(to_df)
+    
+    def select_edges(self, hits1, hits2, layer1, layer2):
+        hit_pairs = (
+            hits1.reset_index().merge(hits2.reset_index(), on="evtid", suffixes=("_1", "_2"))
+        )
+        
+        # define various geometric quantities
+        dphi = self.calc_dphi(hit_pairs.phi_1, hit_pairs.phi_2)
+        dz = hit_pairs.z_2 - hit_pairs.z_1
+        dr = hit_pairs.r_2 - hit_pairs.r_1
+        eta_1 = self.calc_eta(hit_pairs.r_1, hit_pairs.z_1)
+        eta_2 = self.calc_eta(hit_pairs.r_2, hit_pairs.z_2)
+        deta = eta_2 - eta_1
+        dR = np.sqrt(deta**2 + dphi**2)
+        du = hit_pairs.u_2 - hit_pairs.u_1
+        dv = hit_pairs.v_2 - hit_pairs.v_1
+        m = dv / du
+        b = hit_pairs.v_1 - m * hit_pairs.u_1
+        u_approach = -m * b / (1 + m**2)
+        v_approach = u_approach * m + b
+        
+        # restrict the distance of closest approach in the uv plane
+        uv_approach = np.sqrt(u_approach**2 + v_approach**2)
+        
+        # restrict phi_slope and z0
+        phi_slope = dphi / dr
+        z0 = hit_pairs.z_1 - hit_pairs.r_1 * dz / dr
+
+        # apply the intersecting line cut
+        intersected_layer = dr.abs() < -1
+        # 0th barrel layer to left EC or right EC
+        if (layer1 == 0) and (layer2 == 11 or layer2 == 4):
+            z_coord = 71.56298065185547 * dz / dr + z0
+            intersected_layer = np.logical_and(z_coord > -490.975, z_coord < 490.975)
+        # 1st barrel layer to the left EC or right EC
+        if (layer1 == 1) and (layer2 == 11 or layer2 == 4):
+            z_coord = 115.37811279296875 * dz / dr + z0
+            intersected_layer = np.logical_and(z_coord > -490.975, z_coord < 490.975)
+            
+        # filter edges according to selection criteria
+        good_edge_mask = (
+            (phi_slope.abs() < self.phi_slope_max)
+            & (z0.abs() < self.z0_max)  # geometric
+            & (dR < self.dR_max)
+            & (uv_approach < self.uv_approach_max)
+            & (intersected_layer == False)
+        )
+        
+        # store edges (in COO format) and geometric edge features
+        selected_edges = pd.DataFrame(
+            {
+                "index_1": hit_pairs["index_1"][good_edge_mask],
+                "index_2": hit_pairs["index_2"][good_edge_mask],
+                "dr": dr[good_edge_mask],
+                "dphi": dphi[good_edge_mask],
+                "dz": dz[good_edge_mask],
+                "dR": dR[good_edge_mask],
+            }
+        )
+
+        return selected_edges
+        
+    def build_edges(self, evt):
+        if self.pixel_only:
+            layer_pairs = [(0,1), (1,2), (2,3), # barrel-barrel
+                           (0, 4), (1, 4), (2, 4), (3, 4),  # barrel-LEC
+                           (0, 11), (1, 11), (2, 11), (3, 11),  # barrel-REC
+                           (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10), # LEC-LEC
+                           (11, 12), (12, 13), (13, 14), (14, 15), (15, 16), (16, 17) # REC-REC
+                          ]
+        else:
+            layer_pairs = []
+        groups = evt.groupby("layer")
+        edges = []
+        for (layer1, layer2) in layer_pairs:
+            try:
+                hits1 = groups.get_group(layer1)
+                hits2 = groups.get_group(layer2)
+            except KeyError as e:
+                continue
+            
+            print(layer1, layer2)
+            edges_layer_pair = self.select_edges(
+                hits1,
+                hits2,
+                layer1,
+                layer2,
+            )
+            edges.append(edges_layer_pair)
+        edges = pd.concat(edges)
+        edge_attr = np.stack(
+            (
+                edges.dr.values / self.feature_scale[0],
+                edges.dphi.values / self.feature_scale[1],
+                edges.dz.values / self.feature_scale[2],
+                edges.dR.values,
+            )
+        )
+        node_idx = np.arange(len(evt['r']))
+        node_idx = pd.Series(node_idx, index=node_idx)
+        edge_start = node_idx.loc[edges.index_1].values
+        edge_end = node_idx.loc[edges.index_2].values
+        edge_index = np.stack((edge_start, edge_end))
+        return edge_index, edge_attr
+        
+    def process(self, n=10**6):
+        infiles = os.listdir(self.indir)
+        for f in infiles:
+            evtid = f.split('.')[0][5:]
+            print(f'Processing {f}')
+            f = join(self.indir, f)
+            evt = torch.load(f)
+            df = self.get_dataframe(evt, evtid)
+            edge_index, edge_attr = self.build_edges(df)
+            evt.edge_index = edge_index
+            evt.edge_attr = edge_attr
+            print(evt)
--- src/gnn_tracking/preprocessing/point_cloud_builder.py
+++ src/gnn_tracking/preprocessing/point_cloud_builder.py
@@ -4,18 +4,19 @@ import os
 from os.path import join
 
 import numpy as np
+import pandas as pd
 import torch
 from torch_geometric.data import Data
 from trackml.dataset import load_event
 
 
-class GraphSectors:
-    def __init__(
-            self,
-            n_sectors,
-            ds=None,
-            di=None
-    ):
+#class GraphSectors:
+#    def __init__(
+#            self,
+#            n_sectors,
+#            ds=None,
+#            di=None
+#    ):
 
 class PointCloudBuilder:
     def __init__(
@@ -23,73 +24,154 @@ class PointCloudBuilder:
         outdir: str,
         indir: str,
         n_sectors: int,
-        redo=False,
+        redo=True,
+        pixel_only=False,
+        sector_di=0.0001,
+        sector_ds=1.1,
+        feature_names = ["r", "phi", "z", "eta_rz", "u", "v", "layer"],
+        feature_scale = np.array([1,1,1,1,1,1,1]),
+        measurement_mode=False,
+        thld=0.5,
+        remove_noise=False
     ):
         self.outdir = outdir
         self.indir = indir
         self.n_sectors = n_sectors
         self.redo = redo
-        self.idx_dict = {}
-        counter = 0
-        for i in range(1000):
-            for j in range(self.n_sectors):
-                self.idx_dict[counter] = (i, j)
+        self.pixel_only = pixel_only
+        self.sector_di=sector_di
+        self.sector_ds=sector_ds
+        self.feature_names = feature_names
+        self.feature_scale = feature_scale # !! important
+        self.measurement_mode = measurement_mode
+        self.thld=thld
+        self.stats={}
+        self.remove_noise=remove_noise
 
         suffix = "-hits.csv.gz"
         self.prefixes, self.exists = [], {}
+        outfiles = os.listdir(outdir)
         for p in os.listdir(self.indir):
             if str(p).endswith(suffix):
                 prefix = str(p).replace(suffix, "")
                 evtid = int(prefix[-9:])
-                if f"data{evtid}_s0.pt" in os.listdir(outdir):
+                if f"data{evtid}_s0.pt" in outfiles:
                     self.exists[evtid] = True
                 else:
                     self.exists[evtid] = False
                 self.prefixes.append(join(indir, prefix))
 
         self.data_list = []
-        self.process()
 
     def calc_eta(self, r, z):
         theta = np.arctan2(r, z)
         return -1.0 * np.log(np.tan(theta / 2.0))
 
+    def restrict_to_pixel(self, hits):
+        pixel_barrel = [(8, 2), (8, 4), (8, 6), (8, 8)]
+        pixel_LEC = [(7, 14), (7, 12), (7, 10), (7, 8), (7, 6), (7, 4), (7, 2)]
+        pixel_REC = [ (9, 2), (9, 4), (9, 6), (9, 8), (9, 10), (9, 12), (9, 14)]
+        pixel_layers = pixel_barrel + pixel_REC + pixel_LEC
+        n_layers = len(pixel_layers)
+        
+        # select barrel layers and assign convenient layer number [0-9]
+        hit_layer_groups = hits.groupby(["volume_id", "layer_id"])
+        hits = pd.concat(
+            [hit_layer_groups.get_group(pixel_layers[i]).assign(layer=i) 
+             for i in range(n_layers)]
+        )
+        return hits
+
     def append_features(self, hits, particles, truth):
         particles["pt"] = np.sqrt(particles.px**2 + particles.py**2)
         particles["eta_pt"] = self.calc_eta(particles.pt, particles.pz)
+        
+        # handle noise
+        truth_noise = truth[["hit_id", "particle_id"]][truth.particle_id==0]
+        truth_noise["pt"] = 0
         truth = truth[["hit_id", "particle_id"]].merge(
-            particles[["particle_id", "pt", "eta_pt", "q", "vx", "vy"]],
-            on="particle_id",
+            particles[["particle_id", "pt", "eta_pt", "q", "vx", "vy"]], on="particle_id"
         )
+        
+        # optionally add noise
+        if not self.remove_noise:
+            truth = truth.append(truth_noise)
+
         hits["r"] = np.sqrt(hits.x**2 + hits.y**2)
         hits["phi"] = np.arctan2(hits.y, hits.x)
         hits["eta_rz"] = self.calc_eta(hits.r, hits.z)
         hits["u"] = hits["x"] / (hits["x"] ** 2 + hits["y"] ** 2)
         hits["v"] = hits["y"] / (hits["x"] ** 2 + hits["y"] ** 2)
         hits = hits[
-            ["hit_id", "r", "phi", "eta_rz", "x", "y", "z", "u", "v", "volume_id"]
+            ["hit_id", "r", "phi", "eta_rz", "x", "y", "z", "u", "v", "volume_id", "layer"]
         ].merge(truth[["hit_id", "particle_id", "pt", "eta_pt"]], on="hit_id")
-        feature_names = ["r", "phi", "z", "eta_rz", "u", "v"]
-        feature_scale = np.array([1000.0, np.pi, 1000.0, 1, 1 / 1000.0, 1 / 1000.0])
+        return hits
+        
+    def sector_hits(self, hits, s):
+        if (self.n_sectors==1): return hits
+        # build sectors in each 2*np.pi/self.n_sectors window
+        theta = s*2*np.pi/self.n_sectors
+        slope = np.arctan(theta)
+        hits['ur'] = hits['u']*np.cos(theta) - hits['v']*np.sin(theta)
+        hits['vr'] = hits['u']*np.sin(theta) + hits['v']*np.cos(theta)
+
+        lower_bound = -self.sector_ds * slope * hits.ur - self.sector_di
+        upper_bound = self.sector_ds * slope * hits.ur + self.sector_di
+        extended_sector = hits[((hits.vr > lower_bound) & 
+                                (hits.vr < upper_bound) & 
+                                (hits.ur > 0))]
+        
+        measurements = {}
+        if self.measurement_mode:
+            sector = hits[((hits.vr > -slope*hits.ur) &
+                           (hits.vr < slope*hits.ur) &
+                           (hits.ur >= 0))]
+            sector_high_pt = sector[sector.pt>self.thld]
+            sector_low_pt = sector[sector.pt<=self.thld]
+            measurements['sector_size'] = len(sector)
+            measurements['extended_sector_size'] = len(extended_sector)
+            measurements['sector_size_ratio'] = len(extended_sector)/len(sector)
+        return extended_sector, measurements
+            
+    def to_pyg_data(self, hits):
         data = Data(
-            x=hits[feature_names].values / feature_scale,
+            x=hits[self.feature_names].values / self.feature_scale,
             particle_id=hits["particle_id"].values,
             pt=hits["pt"].values,
         )
         return data
-
-    def process(self):
+    
+    def process(self, n=10**6, verbose=False):        
         for i, f in enumerate(self.prefixes):
-            s = 0
+            if i>=n: break
+            print(f"Processing {f}")
+
             evtid = int(f[-9:])
-            name = f"data{evtid}_s{s}.pt"
-            if self.exists[evtid] and not self.redo:
-                data = torch.load(join(self.outdir, name))
-                self.data_list.append(data)
-            else:
-                hits, particles, truth = load_event(
-                    f, parts=["hits", "particles", "truth"]
-                )
-                data = self.append_features(hits, particles, truth)
-                torch.save(data, join(self.outdir, name))
-                self.data_list.append(data)
+            hits, particles, truth = load_event(
+                f, parts=["hits", "particles", "truth"]
+            )
+
+            if self.pixel_only:
+                hits = self.restrict_to_pixel(hits)
+            n_hits = len(hits)
+            hits = self.append_features(hits, particles, truth)
+            n_noise = len(hits[hits.particle_id==0])
+            n_sector_hits = 0
+            for s in range(self.n_sectors):
+                name = f"data{evtid}_s{s}.pt"
+                if self.exists[evtid] and not self.redo:
+                    data = torch.load(join(self.outdir, name))
+                    self.data_list.append(data)
+                else:
+                    sector, measurements = self.sector_hits(hits, s)
+                    n_sector_hits += len(sector)
+                    sector = self.to_pyg_data(sector)
+                    outfile = join(self.outdir, name)
+                    if verbose: print(f'...writing {outfile}')
+                    torch.save(sector, outfile)
+                    self.data_list.append(sector)
+            
+            self.stats[evtid] = {'n_hits': n_hits,
+                                 'n_noise': n_noise,
+                                 'n_sector_hits': n_sector_hits}
+            print('Output statistics:', self.stats[evtid])
