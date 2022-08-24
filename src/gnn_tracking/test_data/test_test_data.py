6eab45c396d82fd31abfda7e2091db14525f176e
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-24 17:22:47 -0400

    Moving trackml test data to subfolder

--- src/gnn_tracking/test_data/test_test_data.py
+++ src/gnn_tracking/test_data/test_test_data.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
-from gnn_tracking.test_data import test_data_dir
+from gnn_tracking.test_data import (
+    test_data_dir,
+    trackml_test_data_dir,
+    trackml_test_data_prefix,
+)
 
 
 def test_test_data_dir():
     assert test_data_dir.is_dir()
+    assert trackml_test_data_dir.is_dir()
+    assert list(trackml_test_data_dir.glob(f"{trackml_test_data_prefix}*"))
