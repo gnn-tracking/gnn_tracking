6541fbed4cacebd64d422c75a17d7eddefd9ab2e
Kilian Lieret <kilian.lieret@posteo.de>
2022-09-07 19:44:58 -0400

    Add subdict_with_prefix_stripped

--- src/gnn_tracking/utils/training.py
+++ src/gnn_tracking/utils/training.py
@@ -68,3 +68,9 @@ _P = TypeVar("_P")
 def add_key_prefix(dct: dict[str, _P], prefix: str = "") -> dict[str, _P]:
     """Return a copy of the dictionary with the prefix added to all keys."""
     return {f"{prefix}{k}": v for k, v in dct.items()}
+
+
+def subdict_with_prefix_stripped(dct: dict[str, _P], prefix: str = "") -> dict[str, _P]:
+    """Return a copy of the dictionary for all keys that start with prefix
+    and with the prefix removed from all keys."""
+    return {k[len(prefix) :]: v for k, v in dct.items() if k.startswith(prefix)}
