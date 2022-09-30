a810d485a1ce3203a457459e8f464757bec655a6
GageDeZoort <jgdezoort@gmail.com>
2022-09-30 15:01:26 -0400

    add bias flag to mlp

--- src/gnn_tracking/models/mlp.py
+++ src/gnn_tracking/models/mlp.py
@@ -4,16 +4,16 @@ import torch.nn as nn
 
 
 class MLP(nn.Module):
-    def __init__(self, input_size, output_size, hidden_dim, L=3):
+    def __init__(self, input_size, output_size, hidden_dim, L=3, bias=True):
         super(MLP, self).__init__()
 
         layers = []
-        layers.append(nn.Linear(input_size, hidden_dim))
+        layers.append(nn.Linear(input_size, hidden_dim, bias=bias))
         for _l in range(1, L - 1):
             layers.append(nn.ReLU())
-            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            layers.append(nn.Linear(hidden_dim, hidden_dim, bias=bias))
         layers.append(nn.ReLU())
-        layers.append(nn.Linear(hidden_dim, output_size))
+        layers.append(nn.Linear(hidden_dim, output_size, bias=bias))
         self.layers = nn.ModuleList(layers)
 
     def reset_parameters(self):
