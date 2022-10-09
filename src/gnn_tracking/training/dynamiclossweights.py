Fix auto scaling of loss weights
Avoiding NaNs in scaling; call it every epoch rather than every batch.

src/gnn_tracking/training/dynamiclossweights.py
@@ -6,6 +6,8 @@ from typing import DefaultDict
 
 import numpy as np
 
+from gnn_tracking.utils.log import logger
+
 
 class DynamicLossWeights(ABC):
     def __init__(self, **kwargs):
@@ -50,7 +52,7 @@ class NormalizeEvery(DynamicLossWeights):
     def __init__(
         self,
         every=5,
-        warmup=0,
+        warmup=1,
         initial_weights: dict[str, float] | None = None,
         rolling=5,
     ):
@@ -58,18 +60,27 @@ class NormalizeEvery(DynamicLossWeights):
 
         Args:
             every: Normalize every this many epochs
-            warmup: Don't do anything for this many epochs
+            warmup: Return default values for this many epcohs.
             initial_weights: Initial weights for first iteration
             rolling: Use this many last losses for normalization
         """
         super().__init__()
         self.every = every
+        if warmup < 1:
+            raise ValueError("At least one warmup epoch required!")
         self.warmup = warmup
         if initial_weights is not None:
             self._current_loss_weights.update(initial_weights)
         self._n_epoch = 0
         self._losses = collections.defaultdict(list)
         self._rolling = rolling
+
+    def _get_scaling_config(self, losses: np.ndarray) -> float:
+        v = np.abs(np.mean(losses[-self._rolling :]))
+        if np.isclose(v, 0.0):
+            logger.warning("Loss is 0, not scaling")
+            v = 1
+        return 1 / v
 
     def _step(self, losses: dict[str, float]) -> dict[str, float]:
         self._n_epoch += 1
@@ -80,7 +91,7 @@ class NormalizeEvery(DynamicLossWeights):
         if self._n_epoch % self.every == 0:
             self._current_loss_weights.update(
                 {
-                    k: 1 / (np.array(vs[-self._rolling :])).mean()
+                    k: self._get_scaling_config(np.array(vs))
                     for k, vs in self._losses.items()
                 }
             )

src/gnn_tracking/training/tcn_trainer.py
@@ -199,12 +199,9 @@ class TCNTrainer:
             else:
                 individual_losses[key] = loss
 
-        loss_weights = self._loss_weight_setter.step(
-            {k: v.detach().cpu() for k, v in individual_losses.items()}
-        )
-
         total = sum(
-            loss_weights[k] * individual_losses[k] for k in individual_losses.keys()
+            self._loss_weight_setter[k] * individual_losses[k]
+            for k in individual_losses.keys()
         )
         return total, individual_losses
 
@@ -257,7 +254,6 @@ class TCNTrainer:
 
         """
         self.model.train()
-
         _losses = collections.defaultdict(list)
         for batch_idx, data in enumerate(self.train_loader):
             data = data.to(self.device)
@@ -284,6 +280,7 @@ class TCNTrainer:
                 _losses[key].append(loss.item())
 
         losses = {k: np.nanmean(v) for k, v in _losses.items()}
+        self._loss_weight_setter.step({k: v.detach().cpu() for k, v in losses.items()})
         self.train_loss.append(pd.DataFrame(losses, index=[self._epoch]))
         for hook in self._train_hooks:
             hook(self.model, losses)