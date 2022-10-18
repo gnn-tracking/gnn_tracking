a5f30ce0ac55b89d01daa044eb3097458bb33ebf
Kilian Lieret <kilian.lieret@posteo.de>
2022-10-18 17:19:50 -0400

    Add sphinx setup

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "gnn_tracking"
+copyright = "2022, Gage deZoort, Kilian Lieret"
+author = "Gage deZoort, Kilian Lieret"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.napoleon", "autoapi.extension"]
+
+autoapi_type = "python"
+autoapi_dirs = ["../../src/gnn_tracking"]
+autoapi_ignore = []
+autoapi_python_class_content = "init"
+
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
