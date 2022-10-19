baea65f9dc3459ddb0f556e74e9ae6975c483331
Kilian Lieret <kilian.lieret@posteo.de>
2022-10-19 18:52:20 -0400

    Utility fct to get commit hash of module

---
+++ src/gnn_tracking/utils/versioning.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from os import PathLike
+from types import ModuleType
+
+from gnn_tracking.utils.log import logger
+
+
+# todo: This will fail in a non-editable install
+def get_commit_hash(module: None | ModuleType | str | PathLike = None) -> str:
+    """Get the git commit hash of the current module."""
+    import git
+
+    if module is None:
+        import gnn_tracking
+
+        module = gnn_tracking
+    if isinstance(module, ModuleType):
+        base_path = str(module.__path__)
+    else:
+        base_path = module
+    repo = git.Repo(path=base_path)
+    if repo.is_dirty():
+        logger.warning("Repository is dirty, commit hash may not be accurate.")
+    return repo.head.object.hexsha
