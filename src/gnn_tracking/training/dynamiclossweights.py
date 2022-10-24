ff18c15ed57df1f11ff840c842168594a936af29
Kilian Lieret <kilian.lieret@posteo.de>
2022-10-24 13:13:23 -0400

    More flexible dynamic loss weights

--- src/gnn_tracking/training/dynamiclossweights.py
+++ src/gnn_tracking/training/dynamiclossweights.py
@@ -11,17 +11,22 @@ from gnn_tracking.utils.log import logger
 
 class DynamicLossWeights(ABC):
     def __init__(self, **kwargs):
+        """Abstract base class to implement dynamic loss weights, i.e. weights for
+        the different loss functions that change during the training.
+        """
         self._current_loss_weights: DefaultDict[str, float] = collections.defaultdict(
             lambda: 1.0
         )
 
-    def get(self) -> dict[str, float]:
+    def get(self) -> DefaultDict[str, float]:
+        """Get loss weights"""
         return self._current_loss_weights
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> float:
+        """Get loss weight for specific loss function"""
         return self.get()[item]
 
-    def step(self, losses: dict[str, float]) -> dict[str, float]:
+    def step(self, losses: dict[str, float]) -> DefaultDict[str, float]:
         """Will be called after each epoch.
 
         Args:
@@ -34,69 +39,69 @@ class DynamicLossWeights(ABC):
         return self.get()
 
     @abstractmethod
-    def _step(self, losses: dict[str, float]) -> dict[str, float]:
+    def _step(self, losses: dict[str, float]) -> DefaultDict[str, float]:
         pass
 
 
 class ConstantLossWeights(DynamicLossWeights):
     def __init__(self, loss_weights: dict[str, float] | None = None):
+        """Constant loss weights."""
         super().__init__()
         if loss_weights is not None:
             self._current_loss_weights.update(loss_weights)
 
-    def _step(self, losses: dict[str, float]) -> dict[str, float]:
+    def _step(self, losses: dict[str, float]) -> DefaultDict[str, float]:
         return self._current_loss_weights
 
 
-class NormalizeEvery(DynamicLossWeights):
+class NormalizeAt(DynamicLossWeights):
     def __init__(
         self,
-        every=5,
-        warmup=1,
-        initial_weights: dict[str, float] | None = None,
+        at: list,
+        relative_weights: list[dict[str, float]] | None = None,
         rolling=5,
     ):
-        """
+        """Normalize losses at specific epochs.
 
         Args:
-            every: Normalize every this many epochs
-            warmup: Return default values for this many epcohs.
-            initial_weights: Initial weights for first iteration
+            at: Normalize at these epochs (starting count from 0)
+            relative_weights: Relative weights between different losses: None (all
+                equal), or list of equal length as ``at``. Each list item dictionary
+                with relative weights. Any key that is not present will be set to 1.
             rolling: Use this many last losses for normalization
         """
         super().__init__()
-        self.every = every
-        if warmup < 1:
-            raise ValueError("At least one warmup epoch required!")
-        self.warmup = warmup
-        if initial_weights is not None:
-            self._current_loss_weights.update(initial_weights)
+        self.at = at
+        if relative_weights is not None:
+            if not len(relative_weights) == len(at):
+                raise ValueError("Length of relative_weights must match length of at")
+        self.relative_weights = relative_weights
         self._n_epoch = 0
         self._losses: DefaultDict[str, list[float]] = collections.defaultdict(list)
         self._rolling = rolling
 
-    def _get_scaling_config(self, losses: np.ndarray) -> float:
-        v = np.abs(np.mean(losses[-self._rolling :]))
+    def _get_relative_weights(self, key: str) -> float:
+        if self.relative_weights is None:
+            return 1.0
+        return self.relative_weights[self.at.index(self._n_epoch)].get(key, 1.0)
+
+    def _get_scaling_config(self, key: str) -> float:
+        v = np.abs(np.mean(self._losses[key][-self._rolling :]))
         if np.isclose(v, 0.0):
             logger.warning("Loss is 0, not scaling")
             v = 1
-        scale = 1 / v
+        scale = self._get_relative_weights(key) / v
         assert scale > 0
         if np.isnan(scale):
-            logger.critical("Scaling is NaN!!")
+            logger.critical("Scaling is NaN!")
         return scale
 
-    def _step(self, losses: dict[str, float]) -> dict[str, float]:
-        self._n_epoch += 1
+    def _step(self, losses: dict[str, float]) -> DefaultDict[str, float]:
         for k, v in losses.items():
             self._losses[k].append(v)
-        if self._n_epoch <= self.warmup:
-            return self._current_loss_weights
-        if self._n_epoch % self.every == 0:
+        if self._n_epoch in self.at:
             self._current_loss_weights.update(
-                {
-                    k: self._get_scaling_config(np.array(vs))
-                    for k, vs in self._losses.items()
-                }
+                {k: self._get_scaling_config(k) for k in self._losses}
             )
+        self._n_epoch += 1
         return self._current_loss_weights
