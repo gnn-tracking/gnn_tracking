bfc4ec7f13431be06ab991de322dab1fa5609629
Kilian Lieret <kilian.lieret@posteo.de>
2022-11-03 11:53:37 -0400

    Factor out ModularGraphTCN

--- src/gnn_tracking/models/track_condensation_networks.py
+++ src/gnn_tracking/models/track_condensation_networks.py
@@ -111,53 +111,49 @@ class PointCloudTCN(nn.Module):
         return {"W": None, "H": h_out, "B": beta, "P": None}
 
 
-class GraphTCN(nn.Module):
+class ModularGraphTCN(nn.Module):
     def __init__(
         self,
+        *,
+        ec: nn.Module,
+        hc_in: nn.Module,
         node_indim: int,
         edge_indim: int,
         h_dim=5,
         e_dim=4,
         h_outdim=2,
         hidden_dim=40,
-        L_ec=3,
         L_hc=3,
-        alpha_ec: float = 0.5,
-        alpha_hc: float = 0.5,
         feed_edge_weights=False,
     ):
-        """
+        """General form of track condensation network based on preconstructed graphs
+        with initial step of edge classification.
 
         Args:
-            node_indim:
-            edge_indim:
+            ec: Edge encoder
+            hc_in: Track condensor interaction network
+            node_indim: Node feature dimension
+            edge_indim: Edge feature dimension
             h_dim: node dimension in latent space
             e_dim: edge dimension in latent space
             h_outdim: output dimension in clustering space
             hidden_dim: width of hidden layers in all perceptrons
-            L_ec: message passing depth for edge classifier
             L_hc: message passing depth for track condenser
-            alpha_ec: strength of residual connection for EC
-            alpha_hc: strength of residual connection for HC
             feed_edge_weights: whether to feed edge weights to the track condenser
         """
         super().__init__()
         self.relu = nn.ReLU()
 
-        self.ec = ECForGraphTCN(
-            node_indim=node_indim,
-            edge_indim=edge_indim,
-            h_dim=h_dim,
-            e_dim=e_dim,
-            hidden_dim=hidden_dim,
-            L_ec=L_ec,
-            alpha_ec=alpha_ec,
-        )
+        #: Edge classification network
+        self.ec = ec
+        #: Track condensation network (usually made up of interaction networks)
+        self.hc_in = hc_in
 
-        # specify the track condenser
+        #: Node encoder network for track condenser
         self.hc_node_encoder = MLP(
             node_indim, h_dim, hidden_dim=hidden_dim, L=2, bias=False
         )
+        #: Edge encoder network for track condenser
         self.hc_edge_encoder = MLP(
             edge_indim + int(feed_edge_weights),
             e_dim,
@@ -165,21 +161,13 @@ class GraphTCN(nn.Module):
             L=2,
             bias=False,
         )
-        self.hc_resin = ResIN.identical_in_layers(
-            node_indim=h_dim,
-            edge_indim=e_dim,
-            node_outdim=h_dim,
-            edge_outdim=e_dim,
-            node_hidden_dim=hidden_dim,
-            edge_hidden_dim=hidden_dim,
-            alpha=alpha_hc,
-            n_layers=L_hc,
-        )
 
-        # modules to predict outputs
-        self.B = MLP(h_dim, 1, hidden_dim, L=3)
-        self.X = MLP(h_dim, h_outdim, hidden_dim, L=3)
-        self.P = IN(
+        #: NN to predict beta
+        self.p_beta = MLP(h_dim, 1, hidden_dim, L=3)
+        #: NN to predict cluster coordinates
+        self.p_cluster = MLP(h_dim, h_outdim, hidden_dim, L=3)
+        #: NN to predict track parameters
+        self.p_track_param = IN(
             h_dim,
             e_dim * (L_hc + 1),
             node_outdim=1,
@@ -212,11 +200,85 @@ class GraphTCN(nn.Module):
         # apply the track condenser
         h_hc = self.relu(self.hc_node_encoder(x))
         edge_attr_hc = self.relu(self.hc_edge_encoder(edge_attr))
-        h_hc, _, edge_attrs_hc = self.hc_resin(h_hc, edge_index, edge_attr_hc)
-        beta = torch.sigmoid(self.B(h_hc))
+        h_hc, _, edge_attrs_hc = self.hc_in(h_hc, edge_index, edge_attr_hc)
+        beta = torch.sigmoid(self.p_beta(h_hc))
         # protect against nans
         beta = beta + torch.ones_like(beta) * 10e-9
 
-        h = self.X(h_hc)
-        track_params, _ = self.P(h_hc, edge_index, torch.cat(edge_attrs_hc, dim=1))
+        h = self.p_cluster(h_hc)
+        track_params, _ = self.p_track_param(
+            h_hc, edge_index, torch.cat(edge_attrs_hc, dim=1)
+        )
         return {"W": edge_weights, "H": h, "B": beta, "P": track_params}
+
+
+class GraphTCN(nn.Module):
+    def __init__(
+        self,
+        node_indim: int,
+        edge_indim: int,
+        h_dim=5,
+        e_dim=4,
+        h_outdim=2,
+        hidden_dim=40,
+        L_ec=3,
+        L_hc=3,
+        alpha_ec: float = 0.5,
+        alpha_hc: float = 0.5,
+        feed_edge_weights=False,
+    ):
+        """Particular implementation of `ModularTCN` with `ECForGraphTCN` as
+        edge classification step and several interaction networks as residual layers
+        for the track condensor network.
+
+        Args:
+            node_indim:
+            edge_indim:
+            h_dim: node dimension in latent space
+            e_dim: edge dimension in latent space
+            h_outdim: output dimension in clustering space
+            hidden_dim: width of hidden layers in all perceptrons
+            L_ec: message passing depth for edge classifier
+            L_hc: message passing depth for track condenser
+            alpha_ec: strength of residual connection for EC
+            alpha_hc: strength of residual connection for HC
+            feed_edge_weights: whether to feed edge weights to the track condenser
+        """
+        super().__init__()
+        ec = ECForGraphTCN(
+            node_indim=node_indim,
+            edge_indim=edge_indim,
+            h_dim=h_dim,
+            e_dim=e_dim,
+            hidden_dim=hidden_dim,
+            L_ec=L_ec,
+            alpha_ec=alpha_ec,
+        )
+        hc_in = ResIN.identical_in_layers(
+            node_indim=h_dim,
+            edge_indim=e_dim,
+            node_outdim=h_dim,
+            edge_outdim=e_dim,
+            node_hidden_dim=hidden_dim,
+            edge_hidden_dim=hidden_dim,
+            alpha=alpha_hc,
+            n_layers=L_hc,
+        )
+        self._gtcn = ModularGraphTCN(
+            ec=ec,
+            hc_in=hc_in,
+            node_indim=node_indim,
+            edge_indim=edge_indim,
+            h_dim=h_dim,
+            e_dim=e_dim,
+            h_outdim=h_outdim,
+            hidden_dim=hidden_dim,
+            L_hc=L_hc,
+            feed_edge_weights=feed_edge_weights,
+        )
+
+    def forward(
+        self,
+        data: Data,
+    ) -> dict[str, Tensor]:
+        return self._gtcn.forward(data=data)
