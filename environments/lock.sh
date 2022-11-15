f74edaea22c13dc85294ddf38000914bf7240838
Kilian Lieret <kilian.lieret@posteo.de>
2022-11-15 13:15:33 -0500

    Use Makefile instead of shell script

--- environments/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-set -x
-
-conda-lock -p linux-64 -p win-64 -f default.yml --lockfile default.conda-lock.yml
-conda-lock -p linux-64 -f minimal.yml --lockfile minimal.conda-lock.yml
-conda-lock render minimal.conda-lock.yml -p linux-64 && mv conda-linux-64.lock minimal-linux-64.lock
