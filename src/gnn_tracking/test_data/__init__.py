cfbf0aacb1157eecdb5dd9d4f6b835fba29501e3
Kilian Lieret <kilian.lieret@posteo.de>
2023-03-09 11:12:34 -0500

    Importlib resources

--- src/gnn_tracking/test_data/__init__.py
+++ src/gnn_tracking/test_data/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from gnn_tracking.utils.compat_resource import resources
+from importlib import resources
 
 test_data_dir = Path(resources.files("gnn_tracking.test_data"))  # type: ignore
 
