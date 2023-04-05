cc2e711d3849c7e902ae66db1c16ef18e1d9f4d8
Kilian Lieret <kilian.lieret@posteo.de>
2023-04-05 18:33:00 -0400

    Expanded tests for Pretrained and PerfectECGraphTCN

--- src/gnn_tracking/test_training_integration.py
+++ src/gnn_tracking/test_training_integration.py
@@ -10,15 +10,28 @@ from gnn_tracking.metrics.losses import (
     LossFctType,
     PotentialLoss,
 )
-from gnn_tracking.models.track_condensation_networks import GraphTCN
+from gnn_tracking.models.edge_classifier import ECForGraphTCN
+from gnn_tracking.models.track_condensation_networks import (
+    GraphTCN,
+    PerfectECGraphTCN,
+    PreTrainedECGraphTCN,
+)
 from gnn_tracking.postprocessing.dbscanscanner import dbscan_scan
 from gnn_tracking.training.dynamiclossweights import NormalizeAt
 from gnn_tracking.training.tcn_trainer import TCNTrainer, TrainingTruthCutConfig
 from gnn_tracking.utils.seeds import fix_seeds
 
+_test_train_test_cases = [
+    ("graphtcn", "default"),
+    ("graphtcn", "auto"),
+    ("graphtcn", "default"),
+    ("pretrainedec", "default"),
+    ("perfectec", "default"),
+]
 
-@pytest.mark.parametrize("loss_weights", [("default"), ("auto")])
-def test_train(tmp_path, built_graphs, loss_weights: str) -> None:
+
+@pytest.mark.parametrize("model,loss_weights", _test_train_test_cases)
+def test_train(tmp_path, built_graphs, model: str, loss_weights: str) -> None:
     fix_seeds()
     _, graph_builder = built_graphs
     g = graph_builder.data_list[0]
@@ -43,14 +56,40 @@ def test_train(tmp_path, built_graphs, loss_weights: str) -> None:
     }
 
     # set up a model and trainer
-    model = GraphTCN(
-        node_indim,
-        edge_indim,
-        h_dim=2,
-        hidden_dim=2,
-        L_ec=2,
-        L_hc=2,
-    )
+    if model == "graphtcn":
+        model_ = GraphTCN(
+            node_indim,
+            edge_indim,
+            h_dim=2,
+            hidden_dim=2,
+            L_ec=2,
+            L_hc=2,
+        )
+    elif model == "pretrainedec":
+        ec = ECForGraphTCN(
+            node_indim=node_indim,
+            edge_indim=edge_indim,
+            hidden_dim=2,
+            L_ec=2,
+        )
+        model_ = PreTrainedECGraphTCN(
+            ec,
+            node_indim=node_indim,
+            edge_indim=edge_indim,
+            hidden_dim=2,
+            L_hc=2,
+        )
+    elif model == "perfectec":
+        model_ = PerfectECGraphTCN(
+            node_indim=node_indim,
+            edge_indim=edge_indim,
+            hidden_dim=2,
+            L_hc=2,
+            ec_tpr=0.8,
+            ec_tnr=0.4,
+        )
+    else:
+        raise ValueError(f"Unknown model type {model}")
 
     _loss_weights = None
     if loss_weights == "default":
@@ -61,7 +100,7 @@ def test_train(tmp_path, built_graphs, loss_weights: str) -> None:
         raise ValueError()
 
     trainer = TCNTrainer(
-        model=model,
+        model=model_,
         loaders=loaders,
         loss_functions=loss_functions,
         lr=0.0001,
