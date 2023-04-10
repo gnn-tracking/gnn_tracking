29cd63768c1200935f89fddac5d5afc903fd1188
Kilian Lieret <kilian.lieret@posteo.de>
2023-04-10 19:25:23 -0400

    Don't put data in memory after writing; import guard

--- src/gnn_tracking/preprocessing/build_point_clouds.py
+++ src/gnn_tracking/preprocessing/build_point_clouds.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import os
 
 from point_cloud_builder import PointCloudBuilder
@@ -9,16 +10,19 @@ idx = int(os.environ["SLURM_ARRAY_TASK_ID"])
 indir = "/tigress/jdezoort/codalab/train_1"
 outdir = "/tigress/jdezoort/object_condensation/point_clouds"
 
-pc_builder = PointCloudBuilder(
-    indir=indir,
-    outdir=outdir,
-    n_sectors=32,
-    pixel_only=True,
-    redo=True,
-    measurement_mode=False,
-    sector_di=0,
-    sector_ds=1.3,
-    thld=0.9,
-    log_level=0,
-)
-pc_builder.process(start=idx, stop=idx + 1)
+
+if __name__ == "__main__":
+    pc_builder = PointCloudBuilder(
+        indir=indir,
+        outdir=outdir,
+        n_sectors=32,
+        pixel_only=True,
+        redo=True,
+        measurement_mode=False,
+        sector_di=0,
+        sector_ds=1.3,
+        thld=0.9,
+        log_level=logging.WARNING,
+        collect_data=False,
+    )
+    pc_builder.process(start=idx, stop=idx + 1)
