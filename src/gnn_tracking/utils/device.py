aad11f44b46430740da2a786cfb43ab3981361ea
Kilian Lieret <kilian.lieret@posteo.de>
2023-04-13 12:09:05 -0400

    Allow to set device from env variable

--- src/gnn_tracking/utils/device.py
+++ src/gnn_tracking/utils/device.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
 
+import os
+
 import torch
 
 
 def guess_device(inpt=None):
-    """Return specified device or guess it"""
-    if inpt is None:
-        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    return inpt
+    """Setting the device (cpu/cuda) with the following fallback alternatives
+
+    1. input
+    2. environment variable GNN_TRACKING_DEVICE
+    3. cuda if available, else cpu
+    """
+    if inpt:
+        return inpt
+    if inpt := os.environ.get("GNN_TRACKING_DEVICE"):
+        return torch.device(inpt)
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
