1fb0f5e1201ba1d1d587e517c526ef4a1f4ae3c0
Kilian Lieret <kilian.lieret@posteo.de>
2023-04-25 11:46:01 -0400

    Revert "Cleanup of name setting for logger"
    This reverts commit 532d1ece5ee787fde360d3ab0153d565562a9500.
    
    This was setting the root logger to debug mode, which for example caused
    the matplotlib font manager (which uses the root logger) to throw lots of
    messages. Here, we want to keep our named logger, but just drop the
    name.

--- src/gnn_tracking/utils/log.py
+++ src/gnn_tracking/utils/log.py
@@ -8,7 +8,7 @@ import colorlog
 LOG_DEFAULT_LEVEL = logging.DEBUG
 
 
-def get_logger(name="", level=LOG_DEFAULT_LEVEL):
+def get_logger(name="gnn-tracking", level=LOG_DEFAULT_LEVEL):
     """Sets up global logger."""
     _log = colorlog.getLogger(name)
 
@@ -28,8 +28,14 @@ def get_logger(name="", level=LOG_DEFAULT_LEVEL):
         "ERROR": "red",
         "CRITICAL": "red",
     }
+    #  This is not the same as just setting name="" in the fct arguments.
+    #  This would set the root logger to debug mode, which for example causes
+    #  the matplotlib font manager (which uses the root logger) to throw lots of
+    #  messages. Here, we want to keep our named logger, but just drop the
+    #  name.
+    name_incl = "" if name == "gnn-tracking" else f" {name}"
     formatter = colorlog.ColoredFormatter(
-        f"%(log_color)s[%(asctime)s{name}] %(levelname)s: %(message)s",
+        f"%(log_color)s[%(asctime)s{name_incl}] %(levelname)s: %(message)s",
         log_colors=log_colors,
         datefmt="%H:%M:%S",
     )
