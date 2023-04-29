4fa40649ca9884e1caf0743bb6adaccf3f375b25
Geo Jolly <geojollyc@gmail.com>
2023-04-29 10:07:17 +0530

    Fix issues with test files

--- src/gnn_tracking/test_data/__init__.py
+++ src/gnn_tracking/test_data/__init__.py
@@ -6,4 +6,4 @@ from pathlib import Path
 test_data_dir = Path(resources.files("gnn_tracking.test_data"))  # type: ignore
 
 trackml_test_data_dir = test_data_dir / "trackml"
-trackml_test_data_prefix = "test_event000000001"
+trackml_test_data_prefix = "event000000001"
