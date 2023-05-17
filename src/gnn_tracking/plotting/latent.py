943139d1ff217719fab25729745d0d2e411f64e3
Kilian Lieret <kilian.lieret@posteo.de>
2023-05-17 13:48:54 -0400

    Added SelectedPidsPlot

--- src/gnn_tracking/plotting/latent.py
+++ src/gnn_tracking/plotting/latent.py
@@ -2,12 +2,13 @@
 
 from __future__ import annotations
 
-from typing import Sequence
+from typing import Callable, Sequence
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from gnn_tracking.utils.colors import lighten_color
+from gnn_tracking.utils.log import logger
 
 
 def plot_coordinates_flat(x: np.ndarray, ax: plt.Axes | None = None) -> plt.Axes:
@@ -116,3 +117,150 @@ def plot_selected_pids(
     ax.scatter(x[mask][:, 0], x[mask][:, 1], c=c, label="Hits of selected PIDs", s=2)
     fig.legend()
     return ax
+
+
+def get_color_mapper(
+    selected_values: Sequence, colors: Sequence | None = None
+) -> Callable[[np.ndarray], np.ndarray]:
+    """Get a function that maps values to colors."""
+    if colors is None:
+        colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    if len(selected_values) > len(colors):
+        raise ValueError(f"Only up to {len(colors)} values can be selected.")
+
+    # cm = np.vectorize(colors.__getitem__)
+    # vm = np.vectorize({p.item(): i for i, p in enumerate(selected_values)}.get)
+
+    color_map = {p.item(): colors[i] for i, p in enumerate(selected_values)}
+
+    def mapper(values):
+        return np.array([color_map.get(v.item()) for v in values])
+
+    return mapper
+
+
+class SelectedPidsPlot:
+    def __init__(
+        self,
+        x_latent: np.ndarray,
+        pid: np.ndarray,
+        labels: np.ndarray | None = None,
+        selected_pids: Sequence[int] | None = None,
+        data: np.ndarray | None = None,
+        ec_hit_mask: np.ndarray | None = None,
+    ):
+        self.data = data
+        if ec_hit_mask is None:
+            ec_hit_mask = np.ones_like(pid, dtype=bool)
+        self._ec_hit_mask = ec_hit_mask
+        self.x = x_latent
+        self.pid = pid[self._ec_hit_mask]
+        self.labels = labels
+        if selected_pids is None:
+            logger.warning(
+                "No PIDs selected, using random PIDs (no pt threshold applied). "
+            )
+            selected_pids = np.random.choice(self.pid[self.pid > 0], 10).astype("int64")
+        self.selected_pids = selected_pids
+
+        self._color_mapper = get_color_mapper(selected_pids)
+        self._selected_pid_mask = np.isin(self.pid, self.selected_pids)
+
+        self._phi = self.data.x[self._ec_hit_mask, 3]
+        self._eta = self.data.x[self._ec_hit_mask, 1]
+
+    def get_collateral_mask(self, pid: int) -> np.ndarray:
+        """Mask for hits that are in the same cluster(s) as the hits belonging to this
+        particle ID.
+        """
+        assert self.labels is not None
+        pid_mask = (self.pid == pid).numpy()
+        assoc_labels = np.unique(self.labels[pid_mask])
+        label_mask = np.isin(self.labels, assoc_labels)
+        col_mask = label_mask & (~pid_mask)
+        return col_mask
+
+    @staticmethod
+    def plot_circles(
+        ax: plt.Axes, xs: np.ndarray, ys: np.ndarray, colors, eps=1
+    ) -> None:
+        assert xs.shape == ys.shape
+        for x, y, c in zip(xs, ys, colors):
+            circle = plt.Circle(
+                (x, y), eps, facecolor=lighten_color(c, 0.2), linestyle="none"
+            )
+            ax.add_patch(circle)
+
+    def get_colors(self, pids: np.ndarray | Sequence) -> np.ndarray:
+        return self._color_mapper(pids)
+
+    def plot_selected_pid_latent(self, ax: plt.Axes, plot_circles=False) -> None:
+        # todo: mark condensation point
+        mask = self._selected_pid_mask
+        c = self.get_colors(self.pid[mask])
+        if plot_circles:
+            self.plot_circles(ax, self.x[mask][:, 0], self.x[mask][:, 1], c)
+        ax.scatter(
+            self.x[mask][:, 0],
+            self.x[mask][:, 1],
+            c=c,
+            label="Hits of selected PIDs",
+            s=12,
+        )
+
+    def plot_collateral_latent(self, ax):
+        for pid in self.selected_pids:
+            mask = self.get_collateral_mask(pid)
+            ax.scatter(
+                self.x[mask][:, 0],
+                self.x[mask][:, 1],
+                c=self.get_colors([pid]),
+                alpha=1,
+                label="Collateral",
+                s=12,
+                marker="x",
+            )
+
+    def plot_other_hit_latent(self, ax):
+        mask = self._selected_pid_mask
+        ax.scatter(
+            self.x[~mask][:, 0],
+            self.x[~mask][:, 1],
+            c="silver",
+            alpha=1,
+            label="Other hits",
+            s=2,
+        )
+
+    def plot_selected_pid_ep(self, ax):
+        mask = self._selected_pid_mask
+        ax.scatter(
+            self._phi[mask],
+            self._eta[mask],
+            c=self.get_colors(self.pid[mask]),
+            s=12,
+            label="Selected PIDs",
+        )
+
+    def plot_other_hit_ep(self, ax):
+        mask = ~self._selected_pid_mask
+        ax.scatter(
+            self._phi[mask],
+            self._eta[mask],
+            c="silver",
+            s=2,
+            label="Other hits",
+        )
+
+    def plot_collateral_ep(self, ax):
+        for pid in self.selected_pids:
+            mask = self.get_collateral_mask(pid)
+            ax.scatter(
+                self._phi[mask],
+                self._eta[mask],
+                c=self.get_colors([pid]),
+                alpha=1,
+                s=12,
+                marker="x",
+            )
