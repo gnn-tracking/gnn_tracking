68d5e3c6874b13d03ba5cfa87fa721adc5ae1abe
Kilian Lieret <kilian.lieret@posteo.de>
2023-05-22 14:25:36 -0400

    Add helper function to generate configs

--- src/gnn_tracking/utils/dictionaries.py
+++ src/gnn_tracking/utils/dictionaries.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import TypeVar
+import itertools
+from copy import deepcopy
+from typing import Any, Sequence, TypeVar
 
 _P = TypeVar("_P")
 
@@ -19,3 +21,18 @@ def subdict_with_prefix_stripped(dct: dict[str, _P], prefix: str = "") -> dict[s
     """Return a copy of the dictionary for all keys that start with prefix
     and with the prefix removed from all keys."""
     return {k[len(prefix) :]: v for k, v in dct.items() if k.startswith(prefix)}
+
+
+def expand_grid(
+    grid: dict[str, Sequence], fixed: dict[str, Sequence] = None
+) -> list[dict[str, Any]]:
+    """Expands a grid of parameters into a list of configurations."""
+    if fixed is None:
+        fixed = {}
+    _configs = list(itertools.product(*grid.values()))
+    configs: list[dict[str, Any]] = []
+    for _c in _configs:
+        c = deepcopy(fixed)
+        c.update({k: v for k, v in zip(grid.keys(), _c)})
+        configs.append(c)
+    return configs
