d9ff5758506ddb86e9e9d6504ed0126cb65c8366
Kilian Lieret <kilian.lieret@posteo.de>
2023-07-24 16:08:29 -0400

    Remove duplicated test

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -69,3 +69,29 @@ def test_ec_plot_integration(built_graphs):
     _, graph_builder = built_graphs
     g = graph_builder.data_list[0].to("cpu")
     get_all_graph_construction_stats(g)
+
+
+def test_fixed_graph_construction(built_graphs):
+    _, graph_builder = built_graphs
+    graphs = graph_builder.data_list
+
+    def _test_for_all(fct, values):
+        for graph, value in zip(graphs, values):
+            assert fct(graph.cpu()) == pytest.approx(value)
+
+    _test_for_all(lambda g: len(g.x), [843, 750])
+    _test_for_all(lambda g: g.x.sum().long(), [15312, -7275])
+    _test_for_all(
+        lambda g: len(g.y),
+        [
+            6752,
+            5796,
+        ],
+    )
+    _test_for_all(
+        lambda g: sum(g.y),
+        [
+            1860,
+            1526,
+        ],
+    )
