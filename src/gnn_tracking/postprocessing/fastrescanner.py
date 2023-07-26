56107514e3fd32c02b7e026e19ebcd880ece4960
Kilian Lieret <kilian.lieret@posteo.de>
2023-07-26 17:41:45 -0400

    Add type hints to DBSCANFastRescan

--- src/gnn_tracking/postprocessing/fastrescanner.py
+++ src/gnn_tracking/postprocessing/fastrescanner.py
@@ -4,28 +4,27 @@ from sklearn.neighbors import NearestNeighbors
 
 
 class DBSCANFastRescan:
-    def __init__(self, x, max_eps=1, *, n_jobs=None):
+    def __init__(
+        self, x: np.ndarray, max_eps: float = 1.0, *, n_jobs: int | None = None
+    ):
         """Class to perform DBSCAN clustering with fast rescanning.
 
         Args:
-            X: Data to cluster
+            x: Data to cluster
             max_eps: Maximum epsilon to use during rescanning. Set to as low
                 as possible to save time.
-            n_jobs : int, default=None
-                The number of parallel jobs to run.
+            n_jobs: The number of parallel jobs to run.
         """
         self.x = x
         self._max_eps = max_eps
-        self.n_jobs = n_jobs
-        self.distances = None
-        self.edges = None
+        self._n_jobs = n_jobs
+        self._distances = None
+        self._edges = None
         self._reset_graph(max_eps)
 
-    def _reset_graph(self, max_eps):
-        """
-        Set and store the radius_neighbors graph to use for clustering.
-        """
-        neighbors_model = NearestNeighbors(radius=max_eps, n_jobs=self.n_jobs)
+    def _reset_graph(self, max_eps: float) -> None:
+        """Set and store the radius_neighbors graph to use for clustering."""
+        neighbors_model = NearestNeighbors(radius=max_eps, n_jobs=self._n_jobs)
         neighbors_model.fit(self.x)
         distances, indexes = neighbors_model.radius_neighbors(
             self.x, radius=max_eps, return_distance=True
@@ -35,10 +34,10 @@ class DBSCANFastRescan:
         source_indexes = np.repeat(np.arange(len(indexes)), lengths)
         target_indexes = np.concatenate(indexes)
 
-        self.distances = np.concatenate(distances)
-        self.edges = np.column_stack((source_indexes, target_indexes))
+        self._distances = np.concatenate(distances)
+        self._edges = np.column_stack((source_indexes, target_indexes))
 
-    def cluster(self, eps=1, min_pts=1):
+    def cluster(self, eps: float = 1.0, min_pts: int = 1):
         """Perform clustering on given data with DBSCAN
 
         Args:
@@ -48,7 +47,7 @@ class DBSCANFastRescan:
         if eps > self._max_eps:
             self._reset_graph(eps)
 
-        filtered_edges = self.edges[self.distances <= eps]
+        filtered_edges = self._edges[self._distances <= eps]
 
         n_neighbors = np.bincount(filtered_edges[:, 0])
         core_samples = np.asarray(n_neighbors >= min_pts, dtype=np.uint8)
