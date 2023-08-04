6e0c38232c7bd3fede6feecda2757d1093362458
Kilian Lieret <kilian.lieret@posteo.de>
2023-08-04 14:17:22 -0400

    Renames: edge_index -> true_edge_index

--- src/gnn_tracking/metrics/losses.py
+++ src/gnn_tracking/metrics/losses.py
@@ -464,7 +464,7 @@ class LossClones(torch.nn.Module):
 def _hinge_loss_components(
     *,
     x: T,
-    edge_index: T,
+    true_edge_index: T,
     particle_id: T,
     pt: T,
     r_emb_hinge: float,
@@ -472,11 +472,11 @@ def _hinge_loss_components(
     p_attr: float,
     p_rep: float,
 ) -> tuple[T, T]:
-    true_edge = (particle_id[edge_index[0]] == particle_id[edge_index[1]]) & (
-        particle_id[edge_index[0]] > 0
+    true_edge = (particle_id[true_edge_index[0]] == particle_id[true_edge_index[1]]) & (
+        particle_id[true_edge_index[0]] > 0
     )
-    true_high_pt_edge = true_edge & (pt[edge_index[0]] > pt_thld)
-    dists = norm(x[edge_index[0]] - x[edge_index[1]], dim=-1)
+    true_high_pt_edge = true_edge & (pt[true_edge_index[0]] > pt_thld)
+    dists = norm(x[true_edge_index[0]] - x[true_edge_index[1]], dim=-1)
     normalization = true_high_pt_edge.sum() + 1e-8
     return torch.sum(
         torch.pow(dists[true_high_pt_edge], p_attr)
@@ -508,8 +508,8 @@ class GraphConstructionHingeEmbeddingLoss(nn.Module, HyperparametersMixin):
         super().__init__()
         self.save_hyperparameters()
 
-    def _build_graph(self, x: T, batch: T, true_edges: T, pt: T) -> T:
-        true_edge_mask = pt[true_edges[0]] > self.hparams.attr_pt_thld
+    def _build_graph(self, x: T, batch: T, true_edge_index: T, pt: T) -> T:
+        true_edge_mask = pt[true_edge_index[0]] > self.hparams.attr_pt_thld
         near_edges = radius_graph(
             x,
             r=self.hparams.r_emb,
@@ -518,17 +518,19 @@ class GraphConstructionHingeEmbeddingLoss(nn.Module, HyperparametersMixin):
             max_num_neighbors=self.hparams.max_num_neighbors,
         )
         return torch.unique(
-            torch.cat([true_edges[:, true_edge_mask], near_edges], dim=-1), dim=-1
+            torch.cat([true_edge_index[:, true_edge_mask], near_edges], dim=-1), dim=-1
         )
 
     # noinspection PyUnusedLocal
     def forward(
-        self, *, x: T, particle_id: T, batch: T, edge_index: T, pt: T, **kwargs
+        self, *, x: T, particle_id: T, batch: T, true_edge_index: T, pt: T, **kwargs
     ) -> dict[str, T]:
-        edge_index = self._build_graph(x=x, batch=batch, true_edges=edge_index, pt=pt)
+        true_edge_index = self._build_graph(
+            x=x, batch=batch, true_edge_index=true_edge_index, pt=pt
+        )
         attr, rep = _hinge_loss_components(
             x=x,
-            edge_index=edge_index,
+            true_edge_index=true_edge_index,
             particle_id=particle_id,
             r_emb_hinge=self.hparams.r_emb,
             pt=pt,
