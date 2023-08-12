148b08e6777449298ce4df43ab745c19df02c8a1
Kilian Lieret <kilian.lieret@posteo.de>
2023-08-11 20:25:23 -0400

    Add tracking metrics vs pt / eta

--- tests/test_clusterscanners.py
+++ tests/test_clusterscanners.py
@@ -1,5 +1,9 @@
 import pytest
 
+from gnn_tracking.metrics.cluster_metrics import (
+    tracking_metrics_vs_eta,
+    tracking_metrics_vs_pt,
+)
 from gnn_tracking.postprocessing.clusterscanner import (
     ClusterScanner,
     CombinedClusterScanner,
@@ -21,7 +25,6 @@ def test_combined_cluster_scanner_empty():
 _scanners = [
     DBSCANHyperParamScanner(n_trials=2, keep_best=1),
     DBSCANHyperParamScannerFixed(trials=[{"eps": 0.5, "min_samples": 1}]),
-    DBSCANPerformanceDetails(eps=0.5, min_samples=1),
 ]
 
 
@@ -29,3 +32,11 @@ _scanners = [
 def test_clusterscanner(test_graph, scanner: ClusterScanner):
     scanner(test_graph, {"H": test_graph.x}, 0)
     scanner.get_foms()
+
+
+def test_performance_details(test_graph):
+    scanner = DBSCANPerformanceDetails(eps=0.5, min_samples=1)
+    scanner(test_graph, {"H": test_graph.x}, 0)
+    h_dfs, c_dfs = scanner.get_results()
+    tracking_metrics_vs_pt(h_dfs, c_dfs, pts=[0.0, 0.5, 1.5], max_eta=4.0)
+    tracking_metrics_vs_eta(h_dfs, c_dfs, pt_thld=0.5, etas=[0.0, 1.0, 4.0])
