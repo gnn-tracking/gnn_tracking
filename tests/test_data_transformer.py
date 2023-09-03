5d319fe1092bb6b3ccae93ac54379539ff28cc43
Kilian Lieret <kilian.lieret@posteo.de>
2023-09-03 09:34:57 +0200

    Fix test and test ECCut

--- tests/test_data_transformer.py
+++ tests/test_data_transformer.py
@@ -1,6 +1,7 @@
 from torch_geometric.data import Data
 
-from gnn_tracking.graph_construction.data_transformer import DataTransformer
+from gnn_tracking.graph_construction.data_transformer import DataTransformer, ECCut
+from gnn_tracking.models.edge_filter import EFMLP
 from gnn_tracking.models.graph_construction import (
     GraphConstructionFCNN,
     MLGraphConstruction,
@@ -25,6 +26,22 @@ def test_data_transformer(test_graph: Data, tmp_path):
     ml_graph_builder.process_directories(
         input_dirs=[graph_test_data_dir],
         output_dirs=[tmp_path / "transformed"],
-        progress=False,
+        _first_only=True,
+    )
+
+
+def test_eccut(test_graph: Data, tmp_path):
+    ec = EFMLP(
+        node_indim=test_graph.num_node_features,
+        edge_indim=test_graph.num_edge_features,
+        hidden_dim=3,
+        depth=1,
+    )
+    dt = DataTransformer(
+        transform=ECCut(ec, thld=0.3),
+    )
+    dt.process_directories(
+        input_dirs=[graph_test_data_dir],
+        output_dirs=[tmp_path / "transformed"],
         _first_only=True,
     )
