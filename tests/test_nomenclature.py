81020191f5bc3572693db8339a20db88558ac223
Kilian Lieret <kilian.lieret@posteo.de>
2023-12-20 12:05:33 -0500

    Rename and expand tests

--- tests/test_nomenclature.py
+++ tests/test_nomenclature.py
@@ -1,6 +1,11 @@
+from gnn_tracking.utils.nomenclature import random_trial_name
 from gnn_tracking.utils.nomenclature import variable_manager as vm
 
 
 def test_variable_manager():
     assert vm["eta"].latex == r"$\eta$"
     assert vm["does_not_exist"].latex == "does_not_exist"
+
+
+def test_coolanme():
+    assert len(random_trial_name()) >= 5
